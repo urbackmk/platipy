4a6bb702cb5c6aa9b9ea435241b8267748e53a76
Meghan Urback <urbackmk@gmail.com>
2013-11-02 16:34:48 -0700

    chrome extension which displays an browser icon if the url points to a docs.python.org page

---
+++ background.js
@@ -0,0 +1,9 @@
+// Called when the url of a tab changes.
+function checkForValidUrl(tabId, changeInfo, tab){
+    if (tab.url.indexOf('docs.python.org/2/library') > -1){
+        chrome.pageAction.show(tabId);
+    }
+}
+
+// Listen for any changes to the URL of any tab.
+chrome.tabs.onUpdated.addListener(checkForValidUrl);
