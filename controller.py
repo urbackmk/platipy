01db9945b53872eebb854291a395307c6b159ff8
Meghan Urback <urbackmk@gmail.com>
2013-11-05 14:17:47 -0800

    writing comments to the database

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, redirect, flash, session, url_for
+from flask import Flask, render_template, request, redirect, flash, session, url_for
 import model
+import datetime
 
 app = Flask(__name__)
 
@@ -7,7 +8,19 @@ app = Flask(__name__)
 def showcomments(sectionid):
     section_comments = model.session.query(model.Comment).filter_by(section_id=sectionid).all()
     return render_template("comments.html", section_comments=section_comments)
-#     return "You're in this section: %s" % sectionid
+
+# hardcoded user id to always be user 1 for now
+@app.route("/<sectionid>", methods=["POST"])
+def makecomment(sectionid):
+    comment = request.form.get("comment")
+    new_comment = model.Comment(comment=comment, section_id=sectionid, user_id=1)
+    model.session.add(new_comment)
+    model.session.commit()
+    return redirect(url_for('showcomments', sectionid=sectionid))
+
+@app.template_filter("datefilter")
+def datefilter(dt):
+    return dt.strftime("%d %B %Y")
 
 if __name__=="__main__":
     app.run(debug=True)
