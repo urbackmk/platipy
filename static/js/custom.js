bcf097179c3e9abb1d99de39174348cecfdc4f26
Meghan Urback <urbackmk@gmail.com>
2013-11-14 11:26:19 -0800

    fixed insert code button bug

--- static/js/custom.js
+++ static/js/custom.js
@@ -2,6 +2,6 @@ var textArea = document.getElementById();
 
 $(document).ready(function(){
     $("#insert_code").click(function(){
-        $('#text_area').val('[code][/code]');
+        $('#text_area').val($('#text_area').val() + '[code][/code]');
     });
 });
