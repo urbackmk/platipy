dcc1d121da0997bfa700e22bef8801eba050d589
Meghan Urback <urbackmk@gmail.com>
2013-11-16 13:23:03 -0800

    added autoresize of comment box

--- static/js/custom.js
+++ static/js/custom.js
@@ -1,8 +1,8 @@
 var textArea = document.getElementById();
 
 $(document).ready(function(){
+    $('#text_area').autosize();
     $("#insert_code").click(function(){
         $('#text_area').val($('#text_area').val() + '[code]\n[/code]');
-        $('#text_area').setCursorPosition(6);
     });
 });
