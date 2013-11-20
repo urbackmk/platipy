6deef9bc3f8b83b56eb89efd9a9216ab6deb3359
Meghan Urback <urbackmk@gmail.com>
2013-11-20 14:08:18 -0800

    now storing and displaying website, page title, section title, and segment text in favorites table and html page

--- controller.py
+++ controller.py
@@ -182,8 +182,7 @@ def favorites():
 
     websites_dict = {}
 
-    # {website: {page title:{section title:[(segment text, segment url)]}}}
-    # {website: {section id:[(segment text, segment url)]}}
+    # {website: {page title:{section title:[(segment text, segment path)]}}}
     for favorite in favorites_list:
         website = favorite.section.html_section.split("http://")[1].split("/")[0]
         segment_path = favorite.section.html_section
@@ -191,19 +190,20 @@ def favorites():
         section_title = favorite.section.section_title
         segment_text = favorite.section.segment_text
 
+        section_dict = {section_title: [(segment_text, segment_path)]}
+
         # if there is a website entry:
         if websites_dict.get(website):
         
-            # if there is a matching section id:
-                # append (segment_text, segment_path)
-                # otherwise set the key as the section id and the value as an empty list
-            websites_dict[website].setdefault(section_title, []).append((segment_text, segment_path))
+            # if there is a matching page title:
+            websites_dict[website].setdefault(page_title, section_dict)
+
+            # if there is a matching section title:
+            websites_dict[website][page_title].setdefault(section_title, []).append((segment_text, segment_path))
 
         # if there is no website key:
-            # set the key as the website and the value as a new dictionary
-            # where the key is the section id and the value is a list of tuples
         else:
-            websites_dict.setdefault(website, {section_title: [(segment_text, segment_path)]})
+            websites_dict.setdefault(website, {page_title: section_dict})
 
     return render_template("favorites.html", websites_dict = websites_dict, github_name = user.github_name)
 
