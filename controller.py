c372f56b1eb719131cbe1c60752e585385e4e71a
Meghan Urback <urbackmk@gmail.com>
2013-11-25 17:51:39 -0800

    updating landing page text

--- controller.py
+++ controller.py
@@ -281,7 +281,7 @@ def datefilter(dt):
 @app.template_filter("detect_url_and_make_link")
 def detect_url_and_make_link(incoming_string):
     pat1 = re.compile(r"(^|[\n ])(([\w]+?://[\w\#$%&~.\-;:=,?@\[\]+]*)(/[\w\#$%&~/.\-;:=,?@\[\]+]*)?)", re.IGNORECASE | re.DOTALL)
-    pat2 = re.compile(r"#(^|[\n ])(((www|ftp)\.[\w\#$%&~.\-;:=,?@\[\]+]*)(/[\w\#$%&~/.\-;:=,?@\[\]+]*)?)", re.IGNORECASE | re.DOTALL)
+    pat2 = re.compile(r"(^|[\n ])(((www|ftp)\.[\w\#$%&~.\-;:=,?@\[\]+]*)(/[\w\#$%&~/.\-;:=,?@\[\]+]*)?)", re.IGNORECASE | re.DOTALL)
 
     incoming_string = pat1.sub(r'\1<a href="\2" target="_blank">\3</a>', incoming_string)
     incoming_string = pat2.sub(r'\1<a href="http:/\2" target="_blank">\3</a>', incoming_string)
